885235db44a430198f7e35a8e45941cd403565e8
Hernan G. Arango <arango@marine.rutgers.edu>
2009-03-02 18:04:03 +0000

    src:ticket:287

--- ROMS/Nonlinear/ecosim.h
+++ ROMS/Nonlinear/ecosim.h
@@ -33,9 +33,15 @@
 !      Numerical  simulation  of  apparent  and  inherent optical      !
 !      properties, Deep-Sea Res., 46, 271-317                          !
 !                                                                      !
-!  NOTES to EcoSim                                                     !
-!  This version uses a descending index for depth that is different    !
-!  than the original coding.                                           !
+!  NOTES to EcoSim:                                                    !
+!                                                                      !
+!  * This version uses a descending index for depth that is different  !
+!    than the original coding.					       !
+!							      	       !
+!  * This version of the code has been modified by Bronwyn Cahill and  !
+!    includes a semi-Lagrangian vertical sinking flux algorithm for    !
+!    fecal material and bio_sediment subroutine which remineralizes    !
+!    particulate nitrogen and returns it into dissolved nitrate pool.  !
 !                                                                      !
 !***********************************************************************
 !
@@ -136,8 +142,13 @@
 !
 !  Local variable declarations.
 !
-      integer :: Iter, Tindex, i, ibio, id, itrc, j, k
+      integer, parameter :: Nsink = 30
+
+      integer :: Iter, Tindex, i, isink, ibio, id, itrc, j, k, ic, ks
       integer :: ibac, iband, idom, ifec, iphy, ipig
+
+      integer, dimension(Nsink) :: idsink
+      integer, dimension(IminS:ImaxS,N(ng)) :: ksource
 
       real(r8), parameter :: MinVal = 0.0_r8
 
@@ -149,6 +160,9 @@
 
       real(r8) :: Het_BAC
       real(r8) :: N_quota, RelDOC1, RelDON1, RelDOP1, RelFe
+      real(r8) :: cff, cff1, cffL, cffR, cu, dltL, dltR
+
+      real(r8), dimension(Nsink) :: Wbio
 
       real(r8), dimension(4) :: Bac_G
 
@@ -170,7 +184,6 @@
       real(r8), dimension(IminS:ImaxS,N(ng)) :: E0_nz
       real(r8), dimension(IminS:ImaxS,N(ng)) :: Ed_nz
       real(r8), dimension(IminS:ImaxS,N(ng)) :: DOC_frac
-      real(r8), dimension(IminS:ImaxS,N(ng)) :: Hz_inv
       real(r8), dimension(IminS:ImaxS,N(ng)) :: NitrBAC
       real(r8), dimension(IminS:ImaxS,N(ng)) :: NH4toNO3
       real(r8), dimension(IminS:ImaxS,N(ng)) :: NtoNBAC
@@ -227,21 +240,84 @@
       real(r8), dimension(IminS:ImaxS,N(ng),NT(ng)) :: Bio_old
       real(r8), dimension(IminS:ImaxS,N(ng),NT(ng)) :: Bio_new
 
+      real(r8), dimension(IminS:ImaxS,0:N(ng)) :: FC
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: Hz_inv
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: Hz_inv2
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: Hz_inv3
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: WL
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: WR
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: bL
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: bR
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: qc
+
 #include "set_bounds.h"
 !
 !=======================================================================
 !  Add EcoSim Source/Sink terms.
 !=======================================================================
 !
-      J_LOOP : DO j=Jstr,Jend
+!  Set internal time-stepping.
+!
+      dtbio=dt(ng)/REAL(BioIter(ng),r8)
+!
+!  Set vertical sinking identification and associated sinking velocity
+!  arrays.
+!
+      ic=1
+      DO ifec=1,Nfec
+        idsink(ic)=iFecN(ifec)
+        Wbio(ic)=WF(ifec,ng)
+        ic=ic+1
+        idsink(ic)=iFecC(ifec)
+        Wbio(ic)=WF(ifec,ng)
+        ic=ic+1
+        idsink(ic)=iFecP(ifec)
+        Wbio(ic)=WF(ifec,ng)
+        ic=ic+1
+        idsink(ic)=iFecS(ifec)
+        Wbio(ic)=WF(ifec,ng)
+        ic=ic+1
+        idsink(ic)=iFecF(ifec)
+        Wbio(ic)=WF(ifec,ng)
+        ic=ic+1
+      END DO
+
+      DO iphy=1,Nphy
+        idsink(ic)=iPhyN(iphy)
+        Wbio(ic)=WS(iphy,ng)
+        ic=ic+1
+        idsink(ic)=iPhyC(iphy)
+        Wbio(ic)=WS(iphy,ng)
+        ic=ic+1
+        idsink(ic)=iPhyP(iphy)
+        Wbio(ic)=WS(iphy,ng)
+        ic=ic+1
+        idsink(ic)=iPhyS(iphy)
+        Wbio(ic)=WS(iphy,ng)
+        ic=ic+1
+        idsink(ic)=iPhyF(iphy)
+        Wbio(ic)=WS(iphy,ng)
+        ic=ic+1
+      END DO
 !
 !-----------------------------------------------------------------------
 !  Compute inverse thickness to avoid repeated divisions.
 !-----------------------------------------------------------------------
 !
+      J_LOOP : DO j=Jstr,Jend
         DO k=1,N(ng)
           DO i=Istr,Iend
             Hz_inv(i,k)=1.0_r8/Hz(i,j,k)
+          END DO
+        END DO
+        DO k=1,N(ng)
+          DO i=Istr,Iend
+            Hz_inv2(i,k)=1.0_r8/(Hz(i,j,k)+Hz(i,j,k+1))
+          END DO
+        END DO
+        DO k=1,N(ng)
+          DO i=Istr,Iend
+            Hz_inv3(i,k)=1.0_r8/(Hz(i,j,k-1)+Hz(i,j,k)+Hz(i,j,k+1))
           END DO
         END DO
 !
@@ -388,8 +464,6 @@
 !  Iterate biology source and sink terms.
 !-----------------------------------------------------------------------
 !
-        dtbio=dt(ng)/REAL(BioIter(ng),r8)
-
         ITER_LOOP : DO Iter=1,BioIter(ng)
  
           DO k=1,N(ng)
@@ -2005,6 +2079,252 @@
               END DO
             END IF
           END DO
+!
+!-----------------------------------------------------------------------
+!  Vertical sinking terms.
+!-----------------------------------------------------------------------
+!
+!  Reconstruct vertical profile of selected biological constituents
+!  "Bio(:,:,isink)" in terms of a set of parabolic segments within each
+!  grid box. Then, compute semi-Lagrangian flux due to sinking.
+!
+          SINK_LOOP: DO isink=1,Nsink
+            itrc=idsink(isink)
+!
+!  Copy concentration of biological particulates into scratch array
+!  "qc" (q-central, restrict it to be positive) which is hereafter
+!  interpreted as a set of grid-box averaged values for biogeochemical
+!  constituent concentration.
+!
+            DO k=1,N(ng)
+              DO i=Istr,Iend
+                qc(i,k)=Bio(i,k,itrc)
+              END DO
+            END DO
+!
+            DO k=N(ng)-1,1,-1
+              DO i=Istr,Iend
+                FC(i,k)=(qc(i,k+1)-qc(i,k))*Hz_inv2(i,k)
+              END DO
+            END DO
+            DO k=2,N(ng)-1
+              DO i=Istr,Iend
+                dltR=Hz(i,j,k)*FC(i,k)
+                dltL=Hz(i,j,k)*FC(i,k-1)
+                cff=Hz(i,j,k-1)+2.0_r8*Hz(i,j,k)+Hz(i,j,k+1)
+                cffR=cff*FC(i,k)
+                cffL=cff*FC(i,k-1)
+!
+!  Apply PPM monotonicity constraint to prevent oscillations within the
+!  grid box.
+!
+                IF ((dltR*dltL).le.0.0_r8) THEN
+                  dltR=0.0_r8
+                  dltL=0.0_r8
+                ELSE IF (ABS(dltR).gt.ABS(cffL)) THEN
+                  dltR=cffL
+                ELSE IF (ABS(dltL).gt.ABS(cffR)) THEN
+                  dltL=cffR
+                END IF
+!
+!  Compute right and left side values (bR,bL) of parabolic segments
+!  within grid box Hz(k); (WR,WL) are measures of quadratic variations.
+!
+!  NOTE: Although each parabolic segment is monotonic within its grid
+!        box, monotonicity of the whole profile is not guaranteed,
+!        because bL(k+1)-bR(k) may still have different sign than
+!        qc(i,k+1)-qc(i,k).  This possibility is excluded,
+!        after bL and bR are reconciled using WENO procedure.
+!
+                cff=(dltR-dltL)*Hz_inv3(i,k)
+                dltR=dltR-cff*Hz(i,j,k+1)
+                dltL=dltL+cff*Hz(i,j,k-1)
+                bR(i,k)=qc(i,k)+dltR
+                bL(i,k)=qc(i,k)-dltL
+                WR(i,k)=(2.0_r8*dltR-dltL)**2
+                WL(i,k)=(dltR-2.0_r8*dltL)**2
+              END DO
+            END DO
+            cff=1.0E-14_r8
+            DO k=2,N(ng)-2
+              DO i=Istr,Iend
+                dltL=MAX(cff,WL(i,k  ))
+                dltR=MAX(cff,WR(i,k+1))
+                bR(i,k)=(dltR*bR(i,k)+dltL*bL(i,k+1))/(dltR+dltL)
+                bL(i,k+1)=bR(i,k)
+              END DO
+            END DO
+            DO i=Istr,Iend
+              FC(i,N(ng))=0.0_r8            ! NO-flux boundary condition
+#if defined LINEAR_CONTINUATION
+              bL(i,N(ng))=bR(i,N(ng)-1)
+              bR(i,N(ng))=2.0_r8*qc(i,N(ng))-bL(i,N(ng))
+#elif defined NEUMANN
+              bL(i,N(ng))=bR(i,N(ng)-1)
+              bR(i,N(ng))=1.5_r8*qc(i,N(ng))-0.5_r8*bL(i,N(ng))
+#else
+              bR(i,N(ng))=qc(i,N(ng))       ! default strictly monotonic
+              bL(i,N(ng))=qc(i,N(ng))       ! conditions
+              bR(i,N(ng)-1)=qc(i,N(ng))
+#endif
+#if defined LINEAR_CONTINUATION
+              bR(i,1)=bL(i,2)
+              bL(i,1)=2.0_r8*qc(i,1)-bR(i,1)
+#elif defined NEUMANN
+              bR(i,1)=bL(i,2)
+              bL(i,1)=1.5_r8*qc(i,1)-0.5_r8*bR(i,1)
+#else
+              bL(i,2)=qc(i,1)               ! bottom grid boxes are
+              bR(i,1)=qc(i,1)               ! re-assumed to be
+              bL(i,1)=qc(i,1)               ! piecewise constant.
+#endif
+            END DO
+!
+!  Apply monotonicity constraint again, since the reconciled interfacial
+!  values may cause a non-monotonic behavior of the parabolic segments
+!  inside the grid box.
+!
+            DO k=1,N(ng)
+              DO i=Istr,Iend
+                dltR=bR(i,k)-qc(i,k)
+                dltL=qc(i,k)-bL(i,k)
+                cffR=2.0_r8*dltR
+                cffL=2.0_r8*dltL
+                IF ((dltR*dltL).lt.0.0_r8) THEN
+                  dltR=0.0_r8
+                  dltL=0.0_r8
+                ELSE IF (ABS(dltR).gt.ABS(cffL)) THEN
+                  dltR=cffL
+                ELSE IF (ABS(dltL).gt.ABS(cffR)) THEN
+                  dltL=cffR
+                END IF
+                bR(i,k)=qc(i,k)+dltR
+                bL(i,k)=qc(i,k)-dltL
+              END DO
+            END DO
+!
+!  After this moment reconstruction is considered complete. The next
+!  stage is to compute vertical advective fluxes, FC. It is expected
+!  that sinking may occurs relatively fast, the algorithm is designed
+!  to be free of CFL criterion, which is achieved by allowing
+!  integration bounds for semi-Lagrangian advective flux to use as
+!  many grid boxes in upstream direction as necessary.
+!
+!  In the two code segments below, WL is the z-coordinate of the
+!  departure point for grid box interface z_w with the same indices;
+!  FC is the finite volume flux; ksource(:,k) is index of vertical
+!  grid box which contains the departure point (restricted by N(ng)).
+!  During the search: also add in content of whole grid boxes
+!  participating in FC.
+!
+            cff=dtbio*ABS(Wbio(isink))
+            DO k=1,N(ng)
+              DO i=Istr,Iend
+                FC(i,k-1)=0.0_r8
+                WL(i,k)=z_w(i,j,k-1)+cff
+                WR(i,k)=Hz(i,j,k)*qc(i,k)
+                ksource(i,k)=k
+              END DO
+            END DO
+            DO k=1,N(ng)
+              DO ks=k,N(ng)-1
+                DO i=Istr,Iend
+                  IF (WL(i,k).gt.z_w(i,j,ks)) THEN
+                    ksource(i,k)=ks+1
+                    FC(i,k-1)=FC(i,k-1)+WR(i,ks)
+                  END IF
+                END DO
+              END DO
+            END DO
+!
+!  Finalize computation of flux: add fractional part.
+!
+            DO k=1,N(ng)
+              DO i=Istr,Iend
+                ks=ksource(i,k)
+                cu=MIN(1.0_r8,(WL(i,k)-z_w(i,j,ks-1))*Hz_inv(i,ks))
+                FC(i,k-1)=FC(i,k-1)+                                    &
+     &                    Hz(i,j,ks)*cu*                                &
+     &                    (bL(i,ks)+                                    &
+     &                     cu*(0.5_r8*(bR(i,ks)-bL(i,ks))-              &
+     &                         (1.5_r8-cu)*                             &
+     &                         (bR(i,ks)+bL(i,ks)-                      &
+     &                          2.0_r8*qc(i,ks))))
+              END DO
+            END DO
+            DO k=1,N(ng)
+              DO i=Istr,Iend
+                Bio(i,k,itrc)=qc(i,k)+(FC(i,k)-FC(i,k-1))*Hz_inv(i,k)
+              END DO
+            END DO
+
+#ifdef BIO_SEDIMENT
+!
+!  Particulate flux reaching the seafloor is remineralized and returned
+!  to the dissolved nitrate pool. Without this conversion, particulate
+!  material falls out of the system. This is a temporary fix to restore
+!  total nitrogen conservation. It will be replaced later by a
+!  parameterization that includes the time delay of remineralization
+!  and dissolved oxygen.
+!
+            DO ifec=1,Nfec
+              IF (itrc.eq.iFecN(ifec)) THEN
+                DO i=Istr,Iend
+                  cff1=FC(i,0)*Hz_inv(i,1)
+                  Bio(i,1,iNO3_)=Bio(i,1,iNO3_)+cff1
+                END DO
+              ELSE IF (itrc.eq.iFecC(ifec)) THEN
+                DO i=Istr,Iend
+                  cff1=FC(i,0)*Hz_inv(i,1)
+                  Bio(i,1,iDIC_)=Bio(i,1,iDIC_)+cff1
+                END DO
+              ELSE IF (itrc.eq.iFecP(ifec)) THEN
+                DO i=Istr,Iend
+                  cff1=FC(i,0)*Hz_inv(i,1)
+                  Bio(i,1,iPO4_)=Bio(i,1,iPO4_)+cff1
+                END DO
+              ELSE IF (itrc.eq.iFecS(ifec)) THEN
+                DO i=Istr,Iend
+                  cff1=FC(i,0)*Hz_inv(i,1)
+                  Bio(i,1,iSiO_)=Bio(i,1,iSiO_)+cff1
+                END DO
+              ELSE IF (itrc.eq.iFecF(ifec)) THEN
+                DO i=Istr,Iend
+                  cff1=FC(i,0)*Hz_inv(i,1)
+                  Bio(i,1,iFeO_)=Bio(i,1,iFeO_)+cff1
+                END DO
+              END IF
+            END DO
+            DO iphy=1,Nphy
+              IF (itrc.eq.iPhyN(iphy)) THEN
+                DO i=Istr,Iend
+                  cff1=FC(i,0)*Hz_inv(i,1)
+                  Bio(i,1,iNO3_)=Bio(i,1,iNO3_)+cff1
+                END DO
+              ELSE IF (itrc.eq.iPhyC(iphy)) THEN
+                DO i=Istr,Iend
+                  cff1=FC(i,0)*Hz_inv(i,1)
+                  Bio(i,1,iDIC_)=Bio(i,1,iDIC_)+cff1
+                END DO
+              ELSE IF (itrc.eq.iPhyP(iphy)) THEN
+                DO i=Istr,Iend
+                  cff1=FC(i,0)*Hz_inv(i,1)
+                  Bio(i,1,iPO4_)=Bio(i,1,iPO4_)+cff1
+                END DO
+              ELSE IF (itrc.eq.iPhyS(iphy)) THEN
+                DO i=Istr,Iend
+                  cff1=FC(i,0)*Hz_inv(i,1)
+                  Bio(i,1,iSiO_)=Bio(i,1,iSiO_)+cff1
+                END DO
+              ELSE IF (itrc.eq.iPhyF(iphy)) THEN
+                DO i=Istr,Iend
+                  cff1=FC(i,0)*Hz_inv(i,1)
+                  Bio(i,1,iFeO_)=Bio(i,1,iFeO_)+cff1
+                END DO
+              END IF
+            END DO
+#endif
+          END DO SINK_LOOP
 !
 !-----------------------------------------------------------------------
 !  Update the tendency arrays
