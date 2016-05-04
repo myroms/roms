b035bf9d8f8b743da572a3f01cdd6b0dae8bad6e
Hernan G. Arango <arango@marine.rutgers.edu>
2016-05-04 21:28:27 +0000

    src:ticket:669

--- ROMS/Include/globaldefs.h
+++ ROMS/Include/globaldefs.h
@@ -3,7 +3,7 @@
 **
 ** svn $Id$
 ********************************************************** Hernan G. Arango ***
-** Copyright (c) 2002-2015 The ROMS/TOMS Group     Alexander F. Shchepetkin  **
+** Copyright (c) 2002-2016 The ROMS/TOMS Group     Alexander F. Shchepetkin  **
 **   Licensed under a MIT/X style license                                    **
 **   See License_ROMS.txt                                                    **
 *******************************************************************************
@@ -70,18 +70,6 @@
 */
 
 #define RHO_SURF
-
-/*
-** Activate criteria for isopycnic diffusion of tracer as maximum
-** density slope or minimum density stratification.  Choose only
-** one option. If neither option is activated, the default criteria
-** is used in the algorithm.
-*/
-
-#if defined MIX_ISO_TS && (defined TS_DIF2 || defined TS_DIF4)
-# undef   MAX_SLOPE
-# undef   MIN_STRAT
-#endif
 
 /*
 ** Turn ON/OFF double precision for real type variables and
