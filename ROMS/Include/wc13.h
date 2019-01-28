560fb376ff8a4576170ebcd4b459de6bcce908f6
Hernan G. Arango <arango@marine.rutgers.edu>
2019-01-28 06:13:04 +0000

    src:ticket:796

--- ROMS/Include/wc13.h
+++ ROMS/Include/wc13.h
@@ -1,7 +1,7 @@
 /*
 ** svn $Id: wc13.h 1748 2018-02-10 03:25:17Z arango $
 *******************************************************************************
-** Copyright (c) 2002-2018 The ROMS/TOMS Group                               **
+** Copyright (c) 2002-2019 The ROMS/TOMS Group                               **
 **   Licensed under a MIT/X style license                                    **
 **   See License_ROMS.txt                                                    **
 *******************************************************************************
