1dd086edec9b12e8272234e84429510e92190d2c
Hernan G. Arango <arango@marine.rutgers.edu>
2026-01-29 15:55:37 -0500

    Updating for Spack-Stack 2.0 with LLVM compilers

--- Compilers/roms_compiler_flags.cmake
+++ Compilers/roms_compiler_flags.cmake
@@ -16,6 +16,8 @@ set( my_cpu    "${CMAKE_SYSTEM_PROCESSOR}" )
 
 if( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
   include( compiler_flags_GNU_Fortran )
+elseif( CMAKE_Fortran_COMPILER_ID MATCHES "IntelLLVM" )
+  include( compiler_flags_IntelLLVM_Fortran )
 elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
   include( compiler_flags_Intel_Fortran )
 else()
